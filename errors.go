a056d27b919d10fcd7193e31fae1961936dabbea
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2017-08-21 00:38:28 +0300

    remove obsolete

--- errors.go
+++
@@ -1,7 +0,0 @@
-package highloadcup
-
-import "errors"
-
-var ErrorNotFound = errors.New("missing item")
-var ErrorMalformed = errors.New("malformed data")
-var ErrorObjectExists = errors.New("object with id exists")
