20fe19484908571ec241343d0119a907c87e2971
Yuri Khrustalev <yuri.khrustalev@gmail.com>
2017-08-20 22:49:22 +0300

    wipe errors

--- repos/errors.go
+++
@@ -1,7 +0,0 @@
-package repos
-
-import "errors"
-
-var ErrorNotFound = errors.New("missing item")
-var ErrorMalformed = errors.New("malformed data")
-var ErrorObjectExists = errors.New("object with id exists")
